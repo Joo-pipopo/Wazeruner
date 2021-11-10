9897ffe41b5900e0175f772d5646f4a0ccb18994
Gabriel L. Tecco <51713625+Tecco2233@users.noreply.github.com>
2021-11-09 21:24:33 -0300

    Add files via upload

--- www/telas de rotas/ibiraRotas/rota.js
+++ www/telas de rotas/ibiraRotas/rota.js
@@ -2,3 +2,7 @@ function desafio() {
     var opcoes = ["CORRER TROTANDO", "CORRER NO MÁXIMO", "CORRER PULANDO", "CORRER EM COOPER"];
     alert(opcoes[Math.floor(Math.random() * opcoes.length)]);
 }
+
+function concluir(){
+    window.location = "../ibiraRotas.html";
+}
